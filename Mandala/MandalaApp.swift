04254d5ef804cb3efba9f22d47b08e500ae222ee
Darpal171 <19051477@saultcollege.ca>
2021-02-28 22:24:00 -0500

    Initial Commit

---
+++ Mandala/MandalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandalaApp.swift
+//  Mandala
+//
+//  Created by user189650 on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MandalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
